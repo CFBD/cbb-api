539b2370a664b04f4090ba3145fe1c05d600f625
Bill Radjewski <radjewwj@gmail.com>
2025-02-04 23:40:30 -0500

    chore: cors fix

--- src/config/middleware/cors.ts
+++ src/config/middleware/cors.ts
@@ -1,21 +1,22 @@
 import cors from 'cors';
 
-const env = process.env.NODE_ENV;
+// const env = process.env.NODE_ENV;
 const corsOrigin =
   process.env.CORS_ORIGIN || 'https://collegebasketballdata.com';
+console.log(`CORS Origin: ${corsOrigin}`);
 
 let corsOptions: cors.CorsOptions = {};
 
-if (env != 'development') {
-  corsOptions = {
-    origin: (requestOrigin, callback) => {
-      if (!requestOrigin || requestOrigin == corsOrigin) {
-        callback(null);
-      } else {
-        callback(new Error(`Not allowed by CORS: ${requestOrigin}`));
-      }
-    },
-  };
-}
+// if (env != 'development') {
+//   corsOptions = {
+//     origin: (requestOrigin, callback) => {
+//       if (!requestOrigin || requestOrigin == corsOrigin) {
+//         callback(null);
+//       } else {
+//         callback(new Error(`Not allowed by CORS: ${requestOrigin}`));
+//       }
+//     },
+//   };
+// }
 
 export default cors(corsOptions);
