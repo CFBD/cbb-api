5cadd82932bd7b955482029752d2578e1ae4229d
bluescar <radjewwj@gmail.com>
2026-09-01 19:18:44 -0400

    chore: performance tweaks

--- src/config/middleware/index.ts
+++ src/config/middleware/index.ts
@@ -1,8 +1,19 @@
 import { default as cors } from './cors';
+import { createConcurrencyLimit } from './concurrency';
 import { checkCallQuotas } from './quotas';
 
+const concurrencyLimit = createConcurrencyLimit([
+  {
+    path: '/lineups/team',
+    methods: ['GET'],
+    maxConcurrent: 2,
+    leaseMs: 75000,
+  },
+]);
+
 export default {
-  standard: [checkCallQuotas],
+  standard: [concurrencyLimit, checkCallQuotas],
+  concurrencyLimit,
   cors,
   checkCallQuotas,
 };
